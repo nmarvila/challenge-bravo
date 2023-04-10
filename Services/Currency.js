c96215d8f308b8631c38f70e298c5d5c4a762a06
Nichollas Marvila <nichollasmarvila@gmail.com>
2023-04-10 16:29:47 -0300

    refactor: change variable declarations

--- Services/Currency.js
+++ Services/Currency.js
@@ -4,31 +4,31 @@ class CurrencyService {
     }
 
     set = async (req, res) => {
-        let from = req.query.from
-        let to = req.query.to
-        let rate = req.query.rate
+        const from = req.query.from
+        const to = req.query.to
+        const rate = req.query.rate
 
         await this.currencyRepository.setRate(from, to, rate)
         return 'OK'
     }
 
     delete = async (req, res) => {
-        let from = req.query.from
-        let to = req.query.to
+        const from = req.query.from
+        const to = req.query.to
 
         await this.currencyRepository.deleteRate(from, to)
         return 'OK'
     }
 
     get = async (req, res) => {
-        let from = req.query.from
-        let to = req.query.to
+        const from = req.query.from
+        const to = req.query.to
 
         if (from != undefined && to != undefined) {
-            let rate = await this.currencyRepository.getRate(from, to)
+            const rate = await this.currencyRepository.getRate(from, to)
             return rate
         } else {
-            let rates = await this.currencyRepository.getRates()
+            const rates = await this.currencyRepository.getRates()
             return rates
         }
     }
