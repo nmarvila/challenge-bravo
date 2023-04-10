7936fff87db0478451d9008cdc024452700b1432
Nichollas Marvila <nichollasmarvila@gmail.com>
2023-04-10 15:51:26 -0300

    refactor: simplified Currency controller logic

--- Controllers/Currency.js
+++ Controllers/Currency.js
@@ -2,37 +2,12 @@ const CurrencyService = require('../Services/Currency')
 const CurrencyRepositoryRedisInstance = require('../Repository/CurrencyRepositoryRedis')
 const currencyService = new CurrencyService(CurrencyRepositoryRedisInstance)
 
-module.exports.callSet = async (req, res) => {
-    let result = await currencyService.set(req, res)
+module.exports.callCurrency = async (req, res, method) => {
+    const result = await currencyService[`${method}`](req, res)
 
     if (result == undefined) {
         res.status(404).send('0')
-    }
-    else {
-        res.status(200).send({data: {result}})
-    }
-    req.connection.destroy()
-}
-
-module.exports.callDelete = async (req, res) => {
-    let result = await currencyService.delete(req, res)
-
-    if (result == undefined) {
-        res.status(404).send('0')
-    }
-    else {
-        res.status(200).send({data: {result}})
-    }
-    req.connection.destroy()
-}
-
-module.exports.callGet = async (req, res) => {
-    let result = await currencyService.get(req, res)
-
-    if (result == undefined) {
-        res.status(404).send('0')
-    }
-    else {
+    } else {
         res.status(200).send({data: {result}})
     }
     req.connection.destroy()
--- Routes/Currency.js
+++ Routes/Currency.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const currencyController = require('../Controllers/Currency')
 
-router.post('/', currencyController.callSet)
-router.delete('/', currencyController.callDelete)
-router.get('/', currencyController.callGet)
+router.post('/', (req, res) => { currencyController.callCurrency(req, res, 'set') })
+router.delete('/', (req, res) => { currencyController.callCurrency(req, res, 'delete') })
+router.get('/', (req, res) => { currencyController.callCurrency(req, res, 'get') })
 
 module.exports = router
