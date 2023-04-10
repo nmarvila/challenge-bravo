9a0f587b0bda11f5d63cfd99757268af4e65128e
Nichollas Marvila <nichollasmarvila@gmail.com>
2023-04-10 16:42:46 -0300

    refactor: change and add variable declarations

--- Services/Rates.js
+++ Services/Rates.js
@@ -3,20 +3,24 @@ const Currency = require('../Model/Currency')
 
 class RatesService {
     getRates = async (currencies) => {
-        let url = `https://economia.awesomeapi.com.br/last/${currencies.join(',')}`
-        let result = await axios.get(url)
+        const url = `https://economia.awesomeapi.com.br/last/${currencies.join(',')}`
+        const result = await axios.get(url)
         let rates = []
 
         Object.entries(result.data).forEach(rate => {
-            rates.push(new Currency(rate[1].code, rate[1].codein, Number.parseFloat(rate[1].ask).toFixed(10)))
-            rates.push(new Currency(rate[1].codein, rate[1].code, (1 / rate[1].ask).toFixed(10)))
+            const rateValue = Number.parseFloat(rate[1].ask).toFixed(10)
+            const reverseRateValue = (1 / rate[1].ask).toFixed(10)
+            rates.push(new Currency(rate[1].code, rate[1].codein, rateValue))
+            rates.push(new Currency(rate[1].codein, rate[1].code, reverseRateValue))
         });
 
-        let rateBTCUSD = Number.parseFloat(rates.find(({from, to}) => from === 'BTC' && to === 'USD').rate)
-        let rateETHUSD = Number.parseFloat(rates.find(({from, to}) => from === 'ETH' && to === 'USD').rate)
+        const BTCUSDRateValue = Number.parseFloat(rates.find(({from, to}) => from === 'BTC' && to === 'USD').rate)
+        const ETHUSDRateValue = Number.parseFloat(rates.find(({from, to}) => from === 'ETH' && to === 'USD').rate)
+        const BTCETHRateValue = (BTCUSDRateValue / ETHUSDRateValue).toFixed(10)
+        const ETHBTCRateValue = (ETHUSDRateValue / BTCUSDRateValue).toFixed(10)
         
-        rates.push(new Currency('BTC', 'ETH', (rateBTCUSD / rateETHUSD).toFixed(10)))
-        rates.push(new Currency('ETH', 'BTC', (rateETHUSD / rateBTCUSD).toFixed(10)))
+        rates.push(new Currency('BTC', 'ETH', BTCETHRateValue))
+        rates.push(new Currency('ETH', 'BTC', ETHBTCRateValue))
 
         return rates
     }
