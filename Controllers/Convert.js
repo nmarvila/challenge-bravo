2edcbc0dd2c733209fbb5d601f7442d3b1b194bb
Nichollas Marvila <nichollasmarvila@gmail.com>
2023-04-10 15:35:34 -0300

    refactor: change variable declarations

--- Controllers/Convert.js
+++ Controllers/Convert.js
@@ -2,8 +2,8 @@ const ConvertService = require('../Services/Convert')
 const CurrencyRepositoryRedisInstance = require('../Repository/CurrencyRepositoryRedis')
 
 module.exports.callConvert = async (req, res) => {
-    let convertService = new ConvertService(CurrencyRepositoryRedisInstance)
-    let result = await convertService.convert(req, res)
+    const convertService = new ConvertService(CurrencyRepositoryRedisInstance)
+    const result = await convertService.convert(req, res)
 
     if (result == undefined) {
         res.status(404).send('0')
