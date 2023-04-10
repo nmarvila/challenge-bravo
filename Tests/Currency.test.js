d15e6b0a7f6d87717a02a1cead2d9d71552db2ee
Nichollas Marvila <nichollasmarvila@gmail.com>
2023-04-10 10:26:29 -0300

    test: create test for GET specific Currency

--- Tests/Currency.test.js
+++ Tests/Currency.test.js
@@ -25,3 +25,11 @@ test('test /currency endpoint to get all the available conversion rates', async 
     expect(response.statusCode).toEqual(200)
     expect(JSON.parse(response.text).data.result.length).toBeGreaterThan(0)
 })
+
+test('test /currency endpoint to get a specific conversion rate (if available)', async () => {
+    const response = await request(url).get('/currency?from=USD&to=BRL')
+    expect(response).not.toEqual(null)
+    expect(response.statusCode).toEqual(200)
+    expect(JSON.parse(response.text).data.result).not.toEqual(undefined)
+    expect(JSON.parse(response.text).data.result).toBeGreaterThan(0)
+})
